b8e9b837e527e7c06ab788cb00baa63ca9cc21f4
kunal699704 <kunal699704@gmail.com>
2023-02-18 00:09:31 +0530

    added app-bar, photo and other details along with font and img

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,79 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey[900],
+        appBar: AppBar(
+          title: const Text("Aadhaar"),
+          backgroundColor: Colors.blueGrey[800],
+        ),
+        body: SizedBox(
+          width: double.infinity,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const CircleAvatar(
+                backgroundImage: AssetImage('images/DefaultDP.jpg'),
+                radius: 80,
+              ),
+              const SizedBox(
+                height: 25,
+              ),
+              const Text("Ramesh Kumar",
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 25,
+                      fontWeight: FontWeight.bold,
+                      fontFamily: "SeymourOne")),
+              const SizedBox(
+                height: 20,
+              ),
+              const Text(
+                "Pune, Maharashtra",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 25,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const Text(
+                "(111222)",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 25,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Container(
+                height: 30,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(14),
+                    color: Colors.white),
+                margin: const EdgeInsets.only(left: 85, right: 85),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Icon(Icons.phone, color: Colors.blueGrey[900]),
+                    VerticalDivider(color: Colors.blueGrey[900]),
+                    Text(
+                      " +91-9087654321",
+                      style: TextStyle(
+                        color: Colors.blueGrey[900],
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
